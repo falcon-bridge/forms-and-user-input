59672fc4facfe615359c4b1e4dcf18eaeafbf241
Aayush <falcon.bridge2000@gmail.com>
2022-01-26 19:17:52 +0530

    replaced the form validity state with a normal variable to better the performance

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,18 +3,15 @@ import { useEffect, useState } from "react";
 const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-  const [formIsValid, setFormIsValid] = useState(false);
 
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
-  useEffect(() => {
-    if (enteredNameIsValid) {
-      setFormIsValid(true);
-    } else {
-      setFormIsValid(false);
-    }
-  }, [enteredNameIsValid]);
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
 
   const nameInputChangeHandler = (event) => {
     if (enteredNameTouched === false) {
